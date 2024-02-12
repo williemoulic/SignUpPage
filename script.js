cd913fae2d181cea3a16af014056f0534dbbeaa3
williemoulic <willie.moulic@gmail.com>
2024-02-12 18:31:48 +0800

    .

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.button-login-popup');
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -8,4 +9,8 @@ registerLink.addEventListener('click', ()=> {
 
 loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
+})
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
 })
